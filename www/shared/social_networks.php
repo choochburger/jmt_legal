c300b690055a6eac74b5aafce66375838aa70a72
Chris Lyons <lyons.chr@gmail.com>
2012-11-18 15:01:07 -0500

    remove tumblr and update twitter icon

--- www/shared/social_networks.php
+++ www/shared/social_networks.php
@@ -6,9 +6,6 @@
         <a href="http://twitter.com/JMTEsq" class="social-icon twitter" target="_blank"></a>
       </div>
       <div class="span1">
-        <a href="http://jmtesq.tumblr.com/" class="social-icon blog" target="_blank"></a>
-      </div>
-      <div class="span1">
         <a href="http://www.facebook.com/pages/The-Law-Office-of-John-M-Trani-PLLC/115119508505241" class="social-icon facebook" target="_blank"></a>
       </div>
       <div class="span1">
