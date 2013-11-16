76734eea80cf5afc8a78d1264a40fc6726fc0a63
Robert Whitney <robertj.whitney@gmail.com>
2013-11-16 16:24:00 -0500

    finishes applying theme

--- www/js/main.js
+++ www/js/main.js
@@ -2,50 +2,32 @@ var JMT = {
   init: function() {
     //this.initNavLinks();
     this.initForm();
+    $('.thank .thanks').hide();
   },
 
   initForm: function() {
     var showError;
 
     $(function() {
-      $("#contact .button").click(function() {
+      $("#contact_form .btn").click(function(e) {
+        e.preventDefault();
+
         var name  = $("#form_name").val(),
             email = $("#form_email").val(),
             text  = $("#msg_text").val(),
             dataString = 'name=' + name + '&email=' + email + '&text=' + text;
-
-        if (!name.length || !email.length || !text.length) {
-          showError('Please fill out all fields.');
-          return false;
-        }
 
         $.ajax({
           type: "POST",
           url: "send_email.php",
           data: dataString,
           success: function(e) {
-            var e = JSON.parse(e);
-            if (e.status === 'error') {
-              showError(e.message);
-              return;
-            }
-
-            $('.email.error').hide();
-            $('.email.success').fadeIn(1000);
-            $('#contact .button').fadeOut();
-          },
-          error: function(e) {
-            showError(e.message);
+            $('#contact_form input').val('')
+            $('#contact_form textarea').val('')
+            $('.thank .thanks').fadeIn(500)
           }
         });
-
-        return false;
       });
-
-      showError = function(msg) {
-          $('.email.error').text(msg)
-                           .fadeIn(1000);
-      };
     });
   }
 };
